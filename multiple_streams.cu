ee739d32aa99d998f6840824f73c6d7d3551f242
Magnar Bjorgve <magnbjor@gmail.com>
2023-11-15 11:19:50 +0100

    make multiple_matrices_routine

--- multiple_streams.cu
+++ multiple_streams.cu
@@ -4,22 +4,28 @@
 #include <vector>
 #include <array>
 
-#define CHECK_CUDA(func)  {                                                \
-  cudaError_t status = (func);                                             \
-  if (status != cudaSuccess) {                                             \
-    printf("CUDA API failed at line %d with error: %s\n",                  \
-           __LINE__, cudaGetErrorString(status));                          \
-    return EXIT_FAILURE;                                                   \
-  }                                                                         \
+#define CHECK_CUDA(func, error_handler) {                                   \
+    cudaError_t status = (func);                                            \
+    if (status != cudaSuccess) {                                            \
+        printf("CUDA API failed at line %d with error: %s\n", __LINE__,     \
+               cudaGetErrorString(status));                                 \
+        error_handler(status);                                              \
+    }                                                                       \
 }
 
-#define CHECK_CUSPARSE(func)  {                                             \
-  cusparseStatus_t status = (func);                                         \
-  if (status != CUSPARSE_STATUS_SUCCESS) {                                  \
-    printf("CUSPARSE API failed at line %d with error.\n",                  \
-           __LINE__);                                                       \
-    return EXIT_FAILURE;                                                    \
-  }                                                                         \
+#define CHECK_CUSPARSE(func, error_handler) {                               \
+    cusparseStatus_t status = (func);                                       \
+    if (status != CUSPARSE_STATUS_SUCCESS) {                                \
+        printf("CUSPARSE API failed at line %d with error %d.\n", __LINE__, \
+               status);                                                     \
+        error_handler(status);                                              \
+    }                                                                       \
+}
+
+void error_handler(int status) {
+    // Free any resources if needed...
+
+    exit(EXIT_FAILURE);
 }
 
 struct MatrixData {
@@ -52,25 +58,7 @@ MatrixData generate_identical_matrices(int num_matrices) {
     return data;
 }
 
-int main(void) {
-  // Define your problem and inputs (matrix and vectors) here.
-
-  // Host problem definition
-  float alpha = 1.0f;
-  float beta = 0.0f;
-
-  auto num_matrices = 5; // use whatever number you want
-  MatrixData data = generate_identical_matrices(num_matrices);
-
-  auto num_rows = data.num_rows;
-  auto num_cols = data.num_cols;
-  auto nnz = data.nnz;
-  auto h_rows = data.h_rows;
-  auto h_columns = data.h_columns;
-  auto h_values = data.h_values;
-  auto hX = data.hX;
-  auto hY = data.hY;
-  auto hY_result = data.hY_result;
+void multiply_matrices(MatrixData& data, int num_matrices, float alpha, float beta) {
 
 
 // Define device arrays/vectors to hold matrices and vector data
@@ -89,12 +77,12 @@ std::vector<size_t> bufferSize(num_matrices);
 
 // Allocate device memory for each matrix and vector
 for (auto i = 0; i < num_matrices; i++) {
-    CHECK_CUDA(cudaMalloc((void**) &dA_rows[i], nnz[i] * sizeof(int)))
-    CHECK_CUDA(cudaMalloc((void**) &dA_columns[i], nnz[i] * sizeof(int)))
-    CHECK_CUDA(cudaMalloc((void**) &dA_values[i], nnz[i] * sizeof(float)))
-    CHECK_CUDA(cudaMalloc((void**) &dX[i], num_cols[i] * sizeof(float)))
-    CHECK_CUDA(cudaMalloc((void**) &dY[i], num_rows[i] * sizeof(float)))
-    CHECK_CUDA(cudaMalloc(&dBuffers[i], bufferSize[i]) )
+    CHECK_CUDA(cudaMalloc((void**) &dA_rows[i], data.nnz[i] * sizeof(int)), error_handler)
+    CHECK_CUDA(cudaMalloc((void**) &dA_columns[i], data.nnz[i] * sizeof(int)), error_handler)
+    CHECK_CUDA(cudaMalloc((void**) &dA_values[i], data.nnz[i] * sizeof(float)), error_handler)
+    CHECK_CUDA(cudaMalloc((void**) &dX[i], data.num_cols[i] * sizeof(float)), error_handler)
+    CHECK_CUDA(cudaMalloc((void**) &dY[i], data.num_rows[i] * sizeof(float)), error_handler)
+    CHECK_CUDA(cudaMalloc(&dBuffers[i], bufferSize[i]), error_handler )
 }
 
 for (int i = 0; i < num_matrices; i++) {
@@ -104,43 +92,43 @@ for (int i = 0; i < num_matrices; i++) {
     cusparseSetStream(handles[i], streams[i]);
 
 // Copy matrices and vectors from host to device asynchronously
-    CHECK_CUDA(cudaMemcpyAsync(dA_rows[i], h_rows[i].data(), nnz[i] * sizeof(int), cudaMemcpyHostToDevice, streams[i]))
-    CHECK_CUDA(cudaMemcpyAsync(dA_columns[i], h_columns[i].data(), nnz[i] * sizeof(int), cudaMemcpyHostToDevice, streams[i]))
-    CHECK_CUDA(cudaMemcpyAsync(dA_values[i], h_values[i].data(), nnz[i] * sizeof(float), cudaMemcpyHostToDevice, streams[i]))
-    CHECK_CUDA(cudaMemcpyAsync(dX[i], hX[i].data(), num_cols[i] * sizeof(float), cudaMemcpyHostToDevice, streams[i]))
-    CHECK_CUDA(cudaMemcpyAsync(dY[i], hY[i].data(), num_rows[i] * sizeof(float), cudaMemcpyHostToDevice, streams[i]))
+    CHECK_CUDA(cudaMemcpyAsync(dA_rows[i], data.h_rows[i].data(), data.nnz[i] * sizeof(int), cudaMemcpyHostToDevice, streams[i]), error_handler)
+    CHECK_CUDA(cudaMemcpyAsync(dA_columns[i], data.h_columns[i].data(), data.nnz[i] * sizeof(int), cudaMemcpyHostToDevice, streams[i]), error_handler)
+    CHECK_CUDA(cudaMemcpyAsync(dA_values[i], data.h_values[i].data(), data.nnz[i] * sizeof(float), cudaMemcpyHostToDevice, streams[i]), error_handler)
+    CHECK_CUDA(cudaMemcpyAsync(dX[i], data.hX[i].data(), data.num_cols[i] * sizeof(float), cudaMemcpyHostToDevice, streams[i]), error_handler)
+    CHECK_CUDA(cudaMemcpyAsync(dY[i], data.hY[i].data(), data.num_rows[i] * sizeof(float), cudaMemcpyHostToDevice, streams[i]), error_handler)
 
     // Create sparse matrix A in CSR format
-    CHECK_CUSPARSE( cusparseCreateCoo(&matA[i], num_rows[i], num_cols[i], nnz[i],
+    CHECK_CUSPARSE( cusparseCreateCoo(&matA[i], data.num_rows[i], data.num_cols[i], data.nnz[i],
                                       dA_rows[i], dA_columns[i], dA_values[i],
                                       CUSPARSE_INDEX_32I,
-                                      CUSPARSE_INDEX_BASE_ZERO, CUDA_R_32F) )
+                                      CUSPARSE_INDEX_BASE_ZERO, CUDA_R_32F), error_handler)
 
     // Create dense vector X
-    CHECK_CUSPARSE( cusparseCreateDnVec(&vecX[i], num_cols[i], dX[i], CUDA_R_32F) )
+    CHECK_CUSPARSE( cusparseCreateDnVec(&vecX[i], data.num_cols[i], dX[i], CUDA_R_32F), error_handler )
 
     // Create dense vector Y
-    CHECK_CUSPARSE( cusparseCreateDnVec(&vecY[i], num_rows[i], dY[i], CUDA_R_32F) )
+    CHECK_CUSPARSE( cusparseCreateDnVec(&vecY[i], data.num_rows[i], dY[i], CUDA_R_32F), error_handler )
 
     // Allocate an external buffer if needed
     CHECK_CUSPARSE( cusparseSpMV_bufferSize(
                                     handles[i], CUSPARSE_OPERATION_NON_TRANSPOSE,
                                     &alpha, matA[i], vecX[i], &beta, vecY[i], CUDA_R_32F,
-                                    CUSPARSE_SPMV_ALG_DEFAULT, &bufferSize[i]) )
+                                    CUSPARSE_SPMV_ALG_DEFAULT, &bufferSize[i]), error_handler )
 
 
     CHECK_CUSPARSE( cusparseSpMV(
                                     handles[i], CUSPARSE_OPERATION_NON_TRANSPOSE,
                                     &alpha, matA[i], vecX[i], &beta, vecY[i], CUDA_R_32F,
-                                    CUSPARSE_SPMV_ALG_DEFAULT, &bufferSize) )
+                                    CUSPARSE_SPMV_ALG_DEFAULT, &bufferSize), error_handler )
 
     // destroy matrix/vector descriptors
-    CHECK_CUSPARSE( cusparseDestroySpMat(matA[i]) )
-    CHECK_CUSPARSE( cusparseDestroyDnVec(vecX[i]) )
-    CHECK_CUSPARSE( cusparseDestroyDnVec(vecY[i]) )
+    CHECK_CUSPARSE( cusparseDestroySpMat(matA[i]), error_handler)
+    CHECK_CUSPARSE( cusparseDestroyDnVec(vecX[i]), error_handler)
+    CHECK_CUSPARSE( cusparseDestroyDnVec(vecY[i]), error_handler)
 
-    CHECK_CUDA(cudaMemcpyAsync(hY[i].data(), dY[i], num_rows[i] * sizeof(float), cudaMemcpyDeviceToHost, streams[i]))
-}
+    CHECK_CUDA(cudaMemcpyAsync(data.hY[i].data(), dY[i], data.num_rows[i] * sizeof(float), cudaMemcpyDeviceToHost, streams[i]), error_handler)
+    }
 
 
   // Synchronize each stream
@@ -151,8 +139,8 @@ for (int i = 0; i < num_matrices; i++) {
   for (int i = 0; i < num_matrices; i++) {
 
     int correct = 1;
-    for (int j = 0; j < num_rows[i]; j++) {
-        if (hY[i][j] != hY_result[i][j]) { // direct floating point comparison is not
+    for (int j = 0; j < data.num_rows[i]; j++) {
+        if (data.hY[i][j] != data.hY_result[i][j]) { // direct floating point comparison is not
             correct = 0;             // reliable
             break;
         }
@@ -171,6 +159,22 @@ for (int i = 0; i < num_matrices; i++) {
   for(auto &stream : streams) {
     cudaStreamDestroy(stream);
   }
+
+
+
+}
+
+int main(void) {
+  // Define your problem and inputs (matrix and vectors) here.
+
+  // Host problem definition
+  float alpha = 1.0f;
+  float beta = 0.0f;
+
+  auto num_matrices = 5; // use whatever number you want
+  MatrixData data = generate_identical_matrices(num_matrices);
+
+  multiply_matrices(data, num_matrices, alpha, beta);
 
   return 0;
 }
